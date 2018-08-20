eef17cc976d0b3619e9569a55301705e0e25306f
Xi Jin <fate_stigma@hotmail.com>
2018-08-20 14:30:27 -0700

    Add dependency as a submodule

---
+++ aif.py
@@ -0,0 +1,7 @@
+import sys
+import os
+aif_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'AIDA-Interchange-Format/python/aida_interchange')
+sys.path.append(aif_path)
+import aifutils
+import aida_rdf_ontologies
+
