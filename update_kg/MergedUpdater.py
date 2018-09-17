d15f09bf0a6198d832407bc486dbb49b0455adcb
Lituta <lituta@outlook.com>
2018-09-17 15:20:24 -0700

    get merge entity info

--- update_kg/MergedUpdater.py
+++ update_kg/MergedUpdater.py
@@ -1,14 +1,6 @@
 from .Updater import Updater
-import json
 
 class MergedUpdater(Updater):
-    def get_json_head(self):
-        ent_q = self.queries['merge_ent_nametypelink.sparql']
-        res = self.select_bindings(ent_q)[1]
-        for x in res:
-            # ?e ?name ?translate ?type ?linkTarget
-        # TODO: cluster
-        pass
 
     def get_cluster(self):
         cluster = self.queries['merge_cluster.sparql']
@@ -23,8 +15,7 @@ class MergedUpdater(Updater):
                 ret[cluster_uri][1].append(x['p']['value'])
             if 'e' in x:
                 ret[cluster_uri][0].append(x['e']['value'])
+        return ret
 
-        with open(self.outputs_prefix + 'cluster.json', 'w') as f:
-            json.dump(ret, f, indent=2)
 
 
