4c2f08d0c8975f3c122d8be5f1a9d3de856224fe
Shinyi Chen <jchen@isi.edu>
2019-04-25 15:38:16 -0700

    Update test runner

--- update_kg/update_kg_repo_name.py
+++ update_kg/update_kg_repo_name.py
@@ -1,12 +1,12 @@
 import sys
 from Updater import Updater
 
-repo_name = sys.argv[1]
+repo_name = 'validation-test'
 
-endpoint = 'http://localhost:7200/repositories/' + repo_name
-output = '/nas/home/dongyul/jl_' + repo_name + '/'
-graph = ''
-has_jl = 'True'
+endpoint = 'http://gaiadev01.isi.edu:7200/repositories/' + repo_name
+output = '/Users/jenniferchen/Documents/AIDA/ta2pipline/store_data/' + repo_name
+graph = 'http://www.isi.edu/clusters1'
+has_jl = True
 print('---')
 print('your endpoint: ', endpoint)
 print('your output: ', output)
@@ -14,14 +14,9 @@ print('your graph: ', graph)
 print('your has jl: ', has_jl)
 print('---')
 
-steps = ['run_delete_ori', 'run_load_jl', 'run_entity_nt', 'run_event_nt', 'run_relation_nt', 'run_insert_proto', 'run_super_edge']
 
-print('\n'.join(['Step %d : %s ' % (i, steps[i]) for i in range(len(steps))]))
-
-input('PLEASE MAKE SURE!!!!!!!!!!!!!!')
-
-up = Updater(endpoint, output, graph, True if has_jl == 'True' else False)
-runs = [up.run_delete_ori, up.run_load_jl, up.run_entity_nt, up.run_event_nt, up.run_relation_nt, up.run_insert_proto, up.run_super_edge]
+up = Updater(endpoint, repo_name, output, graph, has_jl)
+runs = [up.run_delete_ori, up.run_load_jl, up.run_entity_nt, up.run_event_nt, up.run_relation_nt]
 
 for run in runs:
     run()
