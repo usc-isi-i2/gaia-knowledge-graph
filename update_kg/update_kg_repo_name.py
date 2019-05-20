fb4186db694d6e10bfa61ec591d4f00d65289902
Shinyi Chen <jchen@isi.edu>
2019-05-20 12:25:56 -0700

    Update tester

--- update_kg/update_kg_repo_name.py
+++ update_kg/update_kg_repo_name.py
@@ -1,11 +1,11 @@
 import sys
 from Updater import Updater
 
-repo_name = 'validation-test'
+repo_name = 'gaia0416ta2'
 
-endpoint = 'http://gaiadev01.isi.edu:7200/repositories/' + repo_name
+endpoint = 'http://gaiadev01.isi.edu:7201/repositories/' + repo_name
 output = '/Users/jenniferchen/Documents/AIDA/ta2pipline/store_data/' + repo_name
-graph = 'http://www.isi.edu/baseline'
+graph = 'http://www.isi.edu/baseline_20190515_002'
 has_jl = True
 print('---')
 print('your endpoint: ', endpoint)
@@ -16,15 +16,12 @@ print('---')
 
 
 up = Updater(endpoint, repo_name, output, graph, has_jl)
-runs = [up.run_delete_ori,
-        up.run_load_jl,
-        up.run_entity_nt,
-        up.run_event_nt,
-        up.run_relation_nt,
-        up.run_insert_proto,
-        up.run_super_edge]
-
-for run in runs:
-    run()
+up.run_delete_ori()
+up.run_load_jl(entity_clusters='clusters-baseline-20190515-002.jl')
+up.run_entity_nt()
+up.run_event_nt()
+up.run_relation_nt()
+up.run_insert_proto()
+up.run_super_edge()
 
 
